829b7c6394fdd055a9bc77ad95186541e0443ebb
Sareeflonda88 <142931776+Sareeflonda88@users.noreply.github.com>
2024-03-29 20:02:01 +0530

    Update ai.pydeep

--- DAXXMUSIC/plugins/tools/ai.py
+++ DAXXMUSIC/plugins/tools/ai.py
@@ -2,20 +2,30 @@ import random
 import time
 import requests
 from DAXXMUSIC import app
-from config import BOT_USERNAME
-
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
 
-@app.on_message(filters.command(["chatgpt","ai","ask","gpt","solve"],  prefixes=["+", ".", "/", "-", "", "$","#","&"]))
-async def chat_gpt(bot, message):
+BUTTON = InlineKeyboardMarkup(
+       [
+              [
+                     InlineKeyboardButton(
+                            text=f"〆 ᴄʟᴏsᴇ 〆",
+                            callback_data="close",
+                    )
+              ]
+       ]
+)              
+
+@app.on_message(filters.command(["chatgpt","ai","ask","gpt"],  prefixes=["+", ".", "/", "", "$","&"]))
+async def zzchat_gpt(bot, message):
     try:
         start_time = time.time()
         await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
 
         if len(message.command) < 2:
             await message.reply_text(
-                "Example:\n\n/chatgpt Where is TajMahal?"
+                "Example:\n\n/ai How To Set Girlfriend? "
             )
         else:
             a = message.text.split(' ', 1)[1]
@@ -28,8 +38,9 @@ async def chat_gpt(bot, message):
                     end_time = time.time()
                     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
                     await message.reply_text(
-                        f" {x}     \n\n✦ ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➠  [˹𝙳𝙷𝙿𝚁˼](https://t.me/FONT_CHANNEL_01)",",
-                        parse_mode=ParseMode.MARKDOWN
+                        f" {x}  \n\n ✦ ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➠  [˹𝙳𝙷𝙿𝚁˼](https://t.me/FONT_CHANNEL_01)",
+                        parse_mode=ParseMode.MARKDOWN,
+                        reply_markup=BUTTON
                     )
                 else:
                     await message.reply_text("No 'results' key found in the response.")
@@ -37,4 +48,4 @@ async def chat_gpt(bot, message):
                 # Handle any other KeyError that might occur
                 await message.reply_text("Error accessing the response.")
     except Exception as e:
-        await message.reply_text(f"**á´‡Ê€Ê€á´Ê€: {e} ")
+        await message.reply_text(f"**ᴇʀʀᴏʀ: {e} ")
