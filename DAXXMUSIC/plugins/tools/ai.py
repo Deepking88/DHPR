2969e9033aac0db439b92c024b2502818c8df552
Sareeflonda88 <142931776+Sareeflonda88@users.noreply.github.com>
2024-03-29 19:50:10 +0530

    Update ai.pydeep

--- DAXXMUSIC/plugins/tools/ai.py
+++ DAXXMUSIC/plugins/tools/ai.py
@@ -2,19 +2,20 @@ import random
 import time
 import requests
 from DAXXMUSIC import app
+from config import BOT_USERNAME
+
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
 
-@app.on_message(filters.command(["chatgpt","ai","ask","gpt"],  prefixes=["+", ".", "/", "", "$","&"]))
-async def zzchat_gpt(bot, message):
+@app.on_message(filters.command(["chatgpt","ai","ask","gpt","solve"],  prefixes=["+", ".", "/", "-", "", "$","#","&"]))
+async def chat_gpt(bot, message):
     try:
         start_time = time.time()
         await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
 
         if len(message.command) < 2:
             await message.reply_text(
-                "Example:\n\n/ai How To Set Girlfriend? "
+                "Example:\n\n/chatgpt Where is TajMahal?"
             )
         else:
             a = message.text.split(' ', 1)[1]
@@ -27,9 +28,8 @@ async def zzchat_gpt(bot, message):
                     end_time = time.time()
                     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
                     await message.reply_text(
-                        f" {x}  \n\n✦ ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➠  [˹𝙳𝙷𝙿𝚁˼](https://t.me/FONT_CHANNEL_01)",
-                        parse_mode=ParseMode.MARKDOWN,
-                        reply_markup=BUTTON
+                        f" {x}     \n\n✦ ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➠  [˹𝙳𝙷𝙿𝚁˼](https://t.me/FONT_CHANNEL_01)",",
+                        parse_mode=ParseMode.MARKDOWN
                     )
                 else:
                     await message.reply_text("No 'results' key found in the response.")
@@ -37,4 +37,4 @@ async def zzchat_gpt(bot, message):
                 # Handle any other KeyError that might occur
                 await message.reply_text("Error accessing the response.")
     except Exception as e:
-        await message.reply_text(f"**ᴇʀʀᴏʀ: {e} ")
+        await message.reply_text(f"**á´‡Ê€Ê€á´Ê€: {e} ")
