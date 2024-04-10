d996781b78b9e418edaace7d3d1ed995155af712
Sareeflonda88 <142931776+Sareeflonda88@users.noreply.github.com>
2024-04-10 14:45:23 +0530

    Update repo.pyyyd

--- DAXXMUSIC/plugins/bot/repo.py
+++ DAXXMUSIC/plugins/bot/repo.py
@@ -19,7 +19,7 @@ async def start(_, msg):
     buttons = [
         [
           InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/exampurrs"),
-          InlineKeyboardButton("ʀᴇᴘᴏ", url="𝙻𝙾𝙳𝙰 𝙻𝙴 𝙻𝙴 𝙼𝙴𝚁𝙰 🫠🤌🖕"),
+          InlineKeyboardButton("ʀᴇᴘᴏ", url="https://telegra.ph/file/9ebec2c7293e867749434.mp4"),
           ],
     ]
     
